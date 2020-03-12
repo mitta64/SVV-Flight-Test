4c3401d26634372f9df0ea96215807aeb054d5fe
Matthew <matthew23_7@live.nl>
2020-03-12 16:28:06 +0100

    Weight update

--- Matlab_converter.py
+++ Matlab_converter.py
@@ -8,6 +8,9 @@ Created on Sat Mar  7 09:05:12 2020
 import scipy.io as spio
 import numpy as np
 
+#=====================================================================================
+
+
 matlab = spio.loadmat('matlab.mat')
 time = matlab['flightdata'][0][0][47][0][0][0].transpose()
 data = time   
@@ -15,3 +18,6 @@ for i in range(47):
     data = np.concatenate((data, matlab['flightdata'][0][0][i][0][0][0]), axis = 1)
     
 
+#======================================================================================
+
+weight_seats = np.array([[10, 131], [20,131], [30,214], [40,214], [50,251], [60,251], [70,288], [80,288], [100,170]])
