0145b7f25d8f109144d933944bcab052c801cf46
meesb <meesbeumer@hotmail.com>
2020-03-14 11:12:05 +0100

    CG location per time wrt to datum line works. Also checked against proscribed values from manual. Now working on normalizing the cg location wrt MAC

--- Matlab_converter.py
+++ Matlab_converter.py
@@ -24,20 +24,20 @@ for i in range(48):
 
 
 
-def instantanious_weight(t,bem,block,data, weight_seats,weight_bagage): # contains hardcoded passenger weights
+def instantanious_weight(t,bem,block,data, weight_seats): # contains hardcoded passenger weights
 
     fuel_use = data[:, 14] + data[:, 15]  # first  is left engine 2nd is right engine fuel use
 
     fuel_present = block - fuel_use[np.where(t == data[:,0])[0]]
 
-    current_total  =   bem + block + np.sum(weight_seats[:,0]) + np.sum(weight_bagage[:,0]) - fuel_use[np.where(t == data[:,0])[0]]
+    current_total = bem + block + np.sum(weight_seats[:,0])  - fuel_use[np.where(t == data[:,0])[0]]
 
-    zero_fuel_mass = np.sum(weight_seats[:,0]) + np.sum(weight_bagage[:,0]) + bem
-    ramp_mass = bem + block + np.sum(weight_seats[:,0]) + np.sum(weight_bagage[:,0])
+    zero_fuel_mass = np.sum(weight_seats[:,0])  + bem
+    ramp_mass = bem + block + np.sum(weight_seats[:,0])
 
     return current_total, fuel_present, zero_fuel_mass, ramp_mass
 
-def fuel_moment(t,bem,block,weight_seats,weight_bagage):
+def fuel_moment(t,bem,block,weight_seats):
     # load the moment data
 
     path = 'moment_data.csv'
@@ -45,7 +45,9 @@ def fuel_moment(t,bem,block,weight_seats,weight_bagage):
     moment_weight = np.genfromtxt(path, delimiter=",", skip_header=1)
     file.close()
 
-    current_weight, fuel_present , zero_fuel,ramp_mass = instantanious_weight(t, bem, block, data, weight_seats, weight_bagage)
+    moment_weight[:,1] = 100 * moment_weight[:,1]
+
+    current_weight, fuel_present , zero_fuel,ramp_mass = instantanious_weight(t, bem, block, data, weight_seats)
 
     fuel_moment = 0
 
@@ -62,24 +64,26 @@ def fuel_moment(t,bem,block,weight_seats,weight_bagage):
 
                 gradient = (y2 - y1) / (x2 - x1)
                 y_comp = y1 - gradient * x1
-                fuel_moment = (gradient * current_weight + y_comp)*100
+
+                fuel_moment = gradient * fuel_present + y_comp
+
 
     return fuel_moment
 
-def moment_equilibrium(weight_seats,weight_baggage,current_weight,zero_fuel,ramp_mass,fuel_moment_datum,bem,arm_bem):
-    m_seats = np.sum(weight_seats[:,0] * weight_seats[:,1])      # mass in lbs times distance to datum in inch
-    m_baggage = np.sum(weight_baggage[:,0] * weight_baggage[:,1])   # mass in lbs times distance to datum in inch
+def moment_equilibrium(weight_seats,current_weight,zero_fuel,ramp_mass,fuel_moment_datum,bem,block,arm_bem):
+    m_seats = np.sum(weight_seats[:, 0] * weight_seats[:,1])      # mass in lbs times distance to datum in inch
     m_bem = bem*arm_bem
+    m_block = 11705.50 *100 # From the table read of at 4100 lbs
 
-    total_moment_datum = m_seats + m_baggage + m_bem + fuel_moment_datum
+    total_moment_datum = m_seats + m_bem + fuel_moment_datum
     xcg_datum = total_moment_datum / current_weight
 
     # standard locations
     xcg_datum_bem = m_bem/bem
-    xcg_datum_0fuel = (m_seats + m_baggage + m_bem) / zero_fuel
-    xcg_datum_ramp = (m_seats+m_baggage+m_bem+fuel_moment_datum)/ramp_mass
+    xcg_datum_0fuel = (m_seats + m_bem) / zero_fuel
+    xcg_datum_ramp = (m_seats + m_bem + m_block)/ramp_mass
 
-    return xcg_datum,xcg_datum_bem,xcg_datum_0fuel,xcg_datum_ramp
+    return xcg_datum_bem,xcg_datum_0fuel,float(xcg_datum_ramp),float(xcg_datum)
 
 # ========================================= MAIN =======================================================================
 
@@ -92,11 +96,8 @@ t = 9                 # in sec
 weight_seats = np.array([[80, 131], [102, 131], [66, 214], [81, 214], [99, 251], [64, 251], [81, 288], [99, 288], [88, 170]])
 weight_seats[:, 0] = 2.20462262 * weight_seats[:, 0]  # convert weight in kg to lbs
 
-# 1st column  = mass 2nd is the distance in inches to front of aircraft
-weight_baggage = np.array([[0, 74], [0, 321], [0, 338]])  # baggage set to 0
+current_weight,fuel_present, zero_fuel_mass,ramp_mass = instantanious_weight(t, bem, block, data, weight_seats)
+fuel_moment_datum = fuel_moment(t,bem,block,weight_seats)
 
-current_weight,fuel_present, zero_fuel_mass,ramp_mass = instantanious_weight(t, bem, block, data, weight_seats, weight_baggage)
-fuel_moment_datum = fuel_moment(t,bem,block,weight_seats,weight_baggage)
-
-xcg_datum = moment_equilibrium(weight_seats,weight_baggage,current_weight,zero_fuel_mass,ramp_mass,fuel_moment_datum,bem,arm_bem)
+xcg_datum = moment_equilibrium(weight_seats,current_weight,zero_fuel_mass,ramp_mass,fuel_moment_datum,bem,block,arm_bem)
 print(xcg_datum)
