49de032278f9eee4a75a546330df1c7e6f789984
Rik <HJHoogendoorn.student@tudelft.nl>
2020-03-18 11:44:55 +0100

    finding avg weight update

--- checkweights.py
+++ checkweights.py
@@ -13,9 +13,19 @@ Short Period 0:56
 from Matlab_converter import data
 
 time    = data[:,0]
-f_usedl   = data[:,14]*0.45359237 #convert to kg
+f_usedl = data[:,14]*0.45359237 #convert to kg
 f_usedr = data[:,15]*0.45359237 #convert to kg
+f_flowl = data[:,4]*0.45359237/3600 #convert to kg/s
+f_flowr = data[:,5]*0.45359237/3600 #convert to kg/s
 
+
+
+W0 = 14937*0.45359237 #kg
+Weight_flow = []
+for i in range(0,len(time)):
+    Weight_flow.append(W0 - (f_flowl[i] + f_flowr[i])/10)
+    W0 = W0-(f_flowl[i]+f_flowr[i])/10
+    
 W0 = 14937*0.45359237 #kg
 Weight = []
 for i in range(0,len(time)):
