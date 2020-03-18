284b03e6fbe33cf835ad06dadbb9c200872218a2
ThomasKeesom <thomas.keesom@gmail.com>
2020-03-18 09:34:13 +0100

    analytical model v2 toegevoegd, cit_parold file used for this

---
+++ AnalyticalModelv2.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 18 08:30:03 2020
+
+@author: Thomas
+"""
+
+#Imports
+from Cit_parold import *
+from math import *
+import numpy as np
+
+#Short period
+A = 4*muc**2*KY2
+B = -2*muc*(KY2*CZa + Cmadot + Cmq)
+C = CZa*Cmq - 2*muc*Cma
+print(A,B,C)
+
+#General complex eigenvalue form
+Lambda1 = -(B/(2*A)) - 1j*(sqrt(4*A*C - B**2) / (2*A))
+Lambda2 = -(B/(2*A)) + 1j*(sqrt(4*A*C - B**2) / (2*A))
+
+print(Lambda1, Lambda2)
+
+
