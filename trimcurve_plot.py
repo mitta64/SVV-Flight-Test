55a7bebf24c06b3f308d2a45bed00f8a01ca11ec
meesb <meesbeumer@hotmail.com>
2020-03-19 16:58:29 +0100

    plotted elevator deflection during phogoid for the bs Q&A

--- trimcurve_plot.py
+++ trimcurve_plot.py
@@ -1,6 +1,8 @@
 import numpy as np
 from matplotlib import pyplot as plt
-
+import scipy.io as spio
+from Cit_par import *
+'''
 data = np.array([[3.9,	0.6],[4.6,0.2],[5.2,0],[6.2,-0.5],[7.1,-0.9],[8.4,-1.5],[10,-2.2],])
 fig,ax = plt.subplots()
 ax.plot(data[:,0],data[:,1], marker = 'v')
@@ -8,4 +10,19 @@ ax.grid(True)
 plt.xlabel( 'Angle of Attack [deg]')
 plt.ylabel("Elevator deflection [deg]")
 ax.axhline(y=0, color='k')
+plt.show()
+'''
+
+matlab = spio.loadmat('matlab.mat')
+time = matlab['flightdata'][0][0][47][0][0][0].transpose()
+data = time
+for i in range(47):
+    data = np.concatenate((data, matlab['flightdata'][0][0][i][0][0][0]), axis = 1)
+
+time = data[32400:33600,0]
+time = (time)/60
+elevator = data[32400:33600,17]
+plt.plot(time,elevator)
+plt.xlabel('time[min]')
+plt.ylabel('Elevator deflection [deg]')
 plt.show()
