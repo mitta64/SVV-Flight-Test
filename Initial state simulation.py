9f368f667f01ccfcd602837a8218c6599d468dbc
meesb <meesbeumer@hotmail.com>
2020-03-21 16:31:54 +0100

    retry push

--- Initial state simulation.py
+++ Initial state simulation.py
@@ -16,10 +16,8 @@ A = np.array([[-1,1],[-8,0]])
 B = np.array([[2],[2]])
 C = np.array([[1,0],[0,1]])
 D = np.array([[0],[0]])
-t = np.linspace(10,60,500)
-x0 = np.array([[1],[2]])
 
-print(np.linalg.eig(A))
+x0 = np.array([[4],[0.1]])
 
 weight_seats = np.array([[80, 131], [102, 131], [66, 214], [81, 214], [99, 251], [64, 251], [81, 288], [99, 288], [88, 170]])
 weight_seats[:, 0] = 2.20462262 * weight_seats[:, 0]  # convert weight in kg to lbs
@@ -29,8 +27,9 @@ bem = 9165  # in lbs
 # compute weight for each time t
 time = data[:,0]
 mass = instantanious_weight(time,bem, block, data, weight_seats)[0]
+#velocity = data[:,13]
 
-def initial_repsonse(A,B,C,D,t,x0,t0,mass):
+def initial_repsonse(A,B,C,D,t,x0,mass):
     # A = system matrix
     # B = control matrix
     # C = output matrix
@@ -39,10 +38,9 @@ def initial_repsonse(A,B,C,D,t,x0,t0,mass):
     dt = 0.1
     y1 = []
     y2 = []
+    length = np.shape(t)[0]
 
-
-    t = t0
-    for i in range(100):
+    for i in t:
 
         y1.append(float(x[0]))
         y2.append(float(x[1]))
@@ -52,14 +50,14 @@ def initial_repsonse(A,B,C,D,t,x0,t0,mass):
         x = x + dt*x_dot
 
         # update A
-        #A[1][1] = mass[int(t*10)]*0.0001
-        t = t+dt
+        A[1][1] = mass[int(np.where(data[:,0] == i)[0])]*0.00001
+
     return y1,y2
 
-y1,y2 = initial_repsonse(A,B,C,D,t,x0,9.0,mass)
+y1,y2 = initial_repsonse(A,B,C,D,time,x0,mass)
 
 
-x = np.linspace(0,100,100)
 
-plt.plot(x[0:],y1[0:],x[0:],y2[0:])
+
+plt.plot(time[0:20],y1[0:20],time[0:20],y2[0:20])
 plt.show()
