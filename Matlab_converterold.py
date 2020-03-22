8a592364a87992f14e59eb2d7f0068648b35db30
ThomasKeesom <thomas.keesom@gmail.com>
2020-03-22 12:28:57 +0100

    Finshed ValidationPlot script

--- Matlab_converterold.py
+++ Matlab_converterold.py
@@ -8,10 +8,10 @@ Created on Sat Mar  7 09:05:12 2020
 import scipy.io as spio
 import numpy as np
 
-matlab = spio.loadmat('matlab.mat')
-time = matlab['flightdata'][0][0][47][0][0][0].transpose()
+matlab = spio.loadmat('FTISxprt-20200306_flight3.mat')
+time = matlab['flightdata'][0][0][48][0][0][0].transpose()
 data = time   
-for i in range(47):
+for i in range(48):
     data = np.concatenate((data, matlab['flightdata'][0][0][i][0][0][0]), axis = 1)
     
 
--- ValidationPlots.py
+++ ValidationPlots.py
@@ -9,25 +9,30 @@ Created on Sat Mar 21 17:32:11 2020
 from Matlab_converterold import data
 import numpy as np
 import matplotlib.pyplot as plt
+#from Initial_state_simulation import initial_repsonse, A_asym, x0, mass
 
 
 ###Retrieving the assymetric eiegnmotions###
-t_asym = 57*60 #[sec], the time at which the asym eigenmotions are conducted
-t_endasym = 3739
+t_asym = 3514 #[sec], the time at which the asym eigenmotions are conducted
+t_endasym = 3555
 i = list(data[:,0]).index(t_asym) #finding the index at which the asym motions start
 iend = list(data[:,0]).index(t_endasym)
-#16-18 control surface inputs
+#17-19 control surface inputs
 da = data[:,17]
 de = data[:,18]
 dr = data[:,19]
 
-#yew has 29 index in matlabfile
+#yaw has 29 index in matlabfile
 yawasym = data[:,29]
 time = data[:,0]
 
-#plt.plot(time[i:iend], yawasym[i:iend])
-#plt.plot(time[i:iend], dr[i:iend])
-plt.plot(time[i::], yawasym[i::])
-plt.plot(time[i::], dr[i::])
+#Determining the output of the numerical model
+timeused = time[t_asym] - ttime[ts_asym][0] #making sure the function uses the same time as the flight test to calc, starting from 0
+yawratenum = initial_repsonse(A_asym, timeused, x0, mass)
+
+plt.plot(time[i:iend], yawasym[i:iend], label = 'Yaw rate: flight data')
+plt.plot(time[i:iend], dr[i:iend], label = 'Rudder input')
+plt.plot(time[i:iend], yawratenum, label = 'Yaw rate: numerical model') #plotting the yaw rate against the time of the validation data.
+plt.legend()
 plt.show()
 
