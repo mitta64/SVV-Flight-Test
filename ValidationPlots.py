8613c44e794fcfde39ed421142e639511e0d4e39
Thomas Nagels <T.Nagels@student.tudelft.nl>
2020-03-22 14:39:02 +0100

    Validation plots change
    Graphs will show the range using a new function.

--- ValidationPlots.py
+++ ValidationPlots.py
@@ -9,11 +9,14 @@ Created on Sat Mar 21 17:32:11 2020
 from Matlab_converterold import data
 import numpy as np
 import matplotlib.pyplot as plt
-#from Initial_state_simulation import initial_repsonse, A_asym, x0, mass
+from Initial_state_simulation import initial_repsonse, A_asym, x0, mass, A_sym
+
+def createlist(starttime, endtime, x):
+	return np.arange(0, endtime-starttime, x)
 
 
 ###Retrieving the assymetric eiegnmotions###
-t_asym = 3514 #[sec], the time at which the asym eigenmotions are conducted
+t_asym = 3515 #[sec], the time at which the asym eigenmotions are conducted
 t_endasym = 3555
 i = list(data[:,0]).index(t_asym) #finding the index at which the asym motions start
 iend = list(data[:,0]).index(t_endasym)
@@ -26,13 +29,16 @@ dr = data[:,19]
 yawasym = data[:,29]
 time = data[:,0]
 
+
+
 #Determining the output of the numerical model
-timeused = time[t_asym] - ttime[ts_asym][0] #making sure the function uses the same time as the flight test to calc, starting from 0
+#timeused = time[t_asym] - time[t_asym][0] #making sure the function uses the same time as the flight test to calc, starting from 0
+timeused = createlist(t_asym, t_endasym, 0.1)
 yawratenum = initial_repsonse(A_asym, timeused, x0, mass)
 
-plt.plot(time[i:iend], yawasym[i:iend], label = 'Yaw rate: flight data')
-plt.plot(time[i:iend], dr[i:iend], label = 'Rudder input')
-plt.plot(time[i:iend], yawratenum, label = 'Yaw rate: numerical model') #plotting the yaw rate against the time of the validation data.
+plt.plot(createlist(0, t_endasym-t_asym, 0.1), yawasym[i:iend], label = 'Yaw rate: flight data')
+plt.plot(createlist(0, t_endasym-t_asym, 0.1), dr[i:iend], label = 'Rudder input')
+plt.plot(timeused, yawratenum[3], label = 'Yaw rate: numerical model') #plotting the yaw rate against the time of the validation data.
 plt.legend()
 plt.show()
 
