5ebf04990cfa3db595cb84cb9528b93902626b41
ThomasKeesom <thomas.keesom@gmail.com>
2020-03-22 12:08:44 +0100

    Updated ValidationPlots.py: correct matlab and correct indices/

--- ValidationPlots.py
+++ ValidationPlots.py
@@ -10,20 +10,24 @@ from Matlab_converterold import data
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 ###Retrieving the assymetric eiegnmotions###
-t_asym = 3600 + 1*60 #[sec], the time at which the asym eigenmotions are conducted
-t_endasym = 3600 + 7*60
+t_asym = 57*60 #[sec], the time at which the asym eigenmotions are conducted
+t_endasym = 3739
 i = list(data[:,0]).index(t_asym) #finding the index at which the asym motions start
 iend = list(data[:,0]).index(t_endasym)
-#27-29 control surface inputs
+#16-18 control surface inputs
 da = data[:,17]
 de = data[:,18]
 dr = data[:,19]
 
+#yew has 29 index in matlabfile
 yawasym = data[:,29]
 time = data[:,0]
 
-plt.plot(time, yawasym)
+#plt.plot(time[i:iend], yawasym[i:iend])
+#plt.plot(time[i:iend], dr[i:iend])
+plt.plot(time[i::], yawasym[i::])
 plt.plot(time[i::], dr[i::])
 plt.show()
 
