0f9d3ac2fa934444fd9ec5c82aabc3ee1d1a208e
meesb <meesbeumer@hotmail.com>
2020-03-23 11:33:20 +0100

    Phogoid geplot nu ook met control input (weinig to geen invloed op de response)

--- Initial_state_simulation.py
+++ Initial_state_simulation.py
@@ -32,23 +32,15 @@ AOA = data[:,1]
 pitch = data[:,22]
 pitchrate = data[:,27]
 
+control_input_sym = np.radians(data[:,17])
+
 
 #plt.plot(time[31000:35000],velocity[31000:35000],time[31000:35000],pitch[31000:35000])
 #plt.grid(True)
 #plt.show()
 
-mode_motion = "phogoid"
 
-if mode_motion == "phogiod":
-    index_0 = 32502
-    u_init =  161 - velocity[index_0]  #kts
-else:
-    u_init = 0
-    index_0 = 100
-
-
-
-x0 = np.array([[velocity[32502]-161],[np.radians(AOA[32502])],[np.radians(pitch[32502])],[np.radians(pitchrate[32502])]])
+x0_phogoid = np.array([[velocity[32502]-161],[np.radians(AOA[32502])],[np.radians(pitch[32502])],[np.radians(pitchrate[32502])]])
 
 # correct system matrix, asymmetric EOMs
 
@@ -59,7 +51,7 @@ x0 = np.array([[velocity[32502]-161],[np.radians(AOA[32502])],[np.radians(pitch[
 #print(np.linalg.eig(A_sym))
 
 
-def initial_repsonse(mode,t,x0,mass):
+def initial_repsonse(mode,t,x0,input,mass):
     # if mode == 1 --> symetric EOM used
     # if mode == 0 --> asymetric EOM used
 
@@ -69,7 +61,7 @@ def initial_repsonse(mode,t,x0,mass):
     # D = gain matrix
 
     # Redefine matices
-
+    #symetric
     C1 = np.matrix([[-2 * muc * c / V0, 0, 0, 0],
                     [0, (CZadot - 2 * muc) * c / V0, 0, 0],
                     [0, 0, -c / V0, 1],
@@ -92,7 +84,9 @@ def initial_repsonse(mode,t,x0,mass):
                     [-Cmde]])
 
     A_sym = - C1_inv * C2
+    B_sym = C1_inv * C3
 
+    #asymetric
     B1 = np.matrix([[(CYbdot - 2 * mub) * (c / V0), 0, 0, 0],
                     [0, -c / (2 * V0), 0, 0],
                     [0, 0, -4 * mub * KX2 * (c / V0), 4 * mub * KXZ * (c / V0)],
@@ -115,11 +109,14 @@ def initial_repsonse(mode,t,x0,mass):
                     [-Cnda, -Cndr]])
 
     A_asym = - B1_inv * B2
+    B_asym = B1_inv * B3
 
     if mode == 1:
         A = A_sym
+        B = B_sym
     else:
         A = A_asym
+        B = B_asym
 
     x = x0
     dt = 0.1
@@ -130,26 +127,22 @@ def initial_repsonse(mode,t,x0,mass):
     y4 = []
     length = np.shape(t)[0]
 
-    for i in t[0:1200]:
+    for i in range(1200):
 
         y1.append(float(x[0]))
         y2.append(float(x[1]))
         y3.append(float(x[2]))
         y4.append(float(x[3]))
 
+        x_dot = np.dot(A,x) + input[32502+i]*B
 
-        x_dot = np.dot(A,x)
         x = x + dt*x_dot
-
-        # update A
-        #A[1][1] = mass[int(np.where(data[:,0] == i)[0])]*0.00001
 
     return y1,y2,y3,y4
 
-y1,y2,y3,y4 = initial_repsonse(1,time,x0,mass)
+y1,y2,y3,y4 = initial_repsonse(1,time,x0_phogoid,control_input_sym,mass)
 
 time = time[0:1200]
-
 
 plt.plot(time[0:],y1[0:],label='u_numerical')
 #plt.plot(time[0:],y2[0:],label='AOA')
@@ -159,6 +152,7 @@ plt.plot(time[0:],y1[0:],label='u_numerical')
 #compare against flight data
 
 plt.plot(time,u_flight[32502:33702],label='u_flight')
+plt.plot(time,control_input_sym[32502:33702]*57.2957795,label = 'control_input')
 plt.legend()
 plt.show()
 
